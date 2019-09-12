9c73d49462359738c10fa3dbac7cd2099abcde3c
Andi Koh <andikoh@amandakoh-mac-c02t24s1hf1r.corp.improbable.io>
2019-09-12 02:26:40 +0100

    fixed header

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,22 +8,6 @@ const Layout = props => {
     <div className={`site-wrapper ${toggleNav ? `site-head-open` : ``}`}>
       <header className="site-head">
         <div className="site-head-container">
-          <a
-            className="nav-burger"
-            href={`#`}
-            onClick={() => setToggleNav(!toggleNav)}
-          >
-            <div
-              className="hamburger hamburger--collapse"
-              aria-label="Menu"
-              role="button"
-              aria-controls="navigation"
-            >
-              <div className="hamburger-box">
-                <div className="hamburger-inner" />
-              </div>
-            </div>
-          </a>
           <nav id="swup" className="site-head-left">
             <ul className="nav" role="menu">
               <li className="nav-home nav-current" role="menuitem">
@@ -34,11 +18,7 @@ const Layout = props => {
               </li>
             </ul>
           </nav>
-          <div className="site-head-center">
-            <Link className="site-head-logo" to={`/`}>
-              {title}
-            </Link>
-          </div>
+
           <div className="site-head-right">
             <div className="social-links">
               <a
@@ -66,6 +46,29 @@ const Layout = props => {
                 Instagram
               </a>
             </div>
+          </div>
+          <a
+            className="nav-burger"
+            href={`#`}
+            onClick={() => setToggleNav(!toggleNav)}
+          >
+            <div
+              className="hamburger hamburger--collapse"
+              aria-label="Menu"
+              role="button"
+              aria-controls="navigation"
+            >
+              <div className="hamburger-box">
+                <div className="hamburger-inner" />
+              </div>
+            </div>
+          </a>
+        </div>
+        <div className="site-head-logo-container">
+          <div className="site-head-center">
+            <Link className="site-head-logo" to={`/`}>
+              {title}
+            </Link>
           </div>
         </div>
       </header>
